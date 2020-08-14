045ed72cc9bc65b7f6271ec926152d567b283088
EKMJ0322 <67903545+EKMJ0322@users.noreply.github.com>
2020-08-14 22:25:36 +0900

    Update exception.py
    위에는 제가 착각해서 좀 멍청(?)하게 짠 코드입니당 ㅠㅠ 아래 코드로 봐주시면 감사하겠습니다:)

--- exception.py
+++ exception.py
@@ -1,18 +1,6 @@
-try:
-    a = '1'
-    b = '0'
-    print(int(a)/int(b))
-except ZeroDivisionError:
-    print("Error Code: integer division or modulo by zero")
-try:
-    c = '2'
-    d = '$'
-    print(int(c)/int(d))
-except ValueError:
-    print("Error Code: invalid literal for int() with base 10: '$'")
-try: 
-    e = '3'
-    f = '1'
-    print(int(int(e)/int(f)))
-except ZeroDivisionError:
-    print("Error Code: integer division or modulo by zero")
+for i in range(int(input())):
+    a, b = input().split()
+    try:
+        print(int(a) // int(b))
+    except Exception as err:
+        print("Error Code:", err)
